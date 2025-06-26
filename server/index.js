a119ccf46886a87787801f0b93b36a53d100b7f7
manjunagasudha <sudhachikkala5@gmail.com>
2025-06-25 20:43:22 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -70,7 +70,10 @@ server.listen(PORT, () => {
 
 connectDB();
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`🚀 Server running on http://localhost:${port}`);
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
 });
